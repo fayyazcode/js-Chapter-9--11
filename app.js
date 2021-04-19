7243e23a5373c816b79277757e77512681b4bcde
Fayyaz Alam <fayazalam.98@gmail.com>
2021-04-19 12:36:42 -0700

    statement check

--- app.js
+++ app.js
@@ -44,7 +44,7 @@
 
     //Q4***********************
 
-    
+
     // var fuel=+prompt("Enter fuel in Liters")
 
     // if(fuel<=0.25){
@@ -53,3 +53,69 @@
     // else{
     //     alert("Your are good to go")
     // }
+
+
+
+    //Q5*****************************************************
+//a
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// //alert is displayed
+
+
+// //b
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// //alert is not displayed coz b has post increment
+
+// //c
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// //Condition 2 is true post increment
+
+
+
+
+//d
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// //alert showsup
+
+
+
+//e
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+//alert true if statment is true 
+
+
+
+//f
+//  if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+    //car is smaller then car
