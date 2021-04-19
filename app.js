7c5e05d270d07d3d76c3c125895895e3c654bf82
Fayyaz Alam <fayazalam.98@gmail.com>
2021-04-19 12:10:46 -0700

    Question one and two

---
+++ app.js
@@ -0,0 +1,24 @@
+
+//Q1********************
+
+// var city=prompt("Enter your city")
+
+// if(city==="karachi"||city==="KARACHI"){
+//    alert("Welcome to City Of Lights")
+// }
+
+
+
+
+//Q2******************************
+
+    // var gender=prompt("Enter your gender")
+    //     if(gender==="male"){
+    //         alert("Hello Sir")
+    //     }
+    //     else if(gender==="female"){
+    //         alert("Hello Ma'am")
+    //     }
+    //     else{
+    //         alert("Enter correct gender gay ;/")
+    //     }
